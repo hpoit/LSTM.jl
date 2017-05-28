1efd977d180dc26aec68a0a15e5e79c6048f3f10
Kevin Liu <kvtliu@gmail.com>
2017-05-28 14:39:10 -0300

    for math_ops.jl

---
+++ TensorArray_ops.jl
@@ -0,0 +1,24 @@
+type TensorArray
+
+type TensorArrayhandle
+type TensorArrayflow
+
+type dtype
+
+function readt(index::Tensor{Int32}, name=None)
+function gathert(indices::Tensor{T}, name=None)
+function packt(name=None)
+function concatt(name=None)
+function writet(index::Scalar{Int32}, value::Tensor{dtype}, name=None)
+function scattert(indices::Tensor{Int32}, value::Tensor{dtype}, name=None)
+function unpackt(value::Tensor{dtype}, name=None)
+function splitt(value::Tensor{dtype}, lengths::Vector{Int32}, name=None)
+function gradt(source, flow=None, name=None)
+function initt(dtype, size::Scalar{Int32}, dynamic_size::Bool, clear_after_read::Bool, tensor_array_name::String,
+              handle::TensorArrayhandle, flow::TensorArrayflow, infer_shape::Bool, name::String)
+    (handle, tensor_array_name) ? throw(ValueError()) :
+    TensorArrayhandle::Tensor ? : throw(TypeError())
+function closet(name=None)
+function sizet(TensorArray, name=None)
+    size(TensorArray)
+end
